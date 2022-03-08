b716f7b277afc9c6bcaa425e4eb6d33d4f347c0a
Jaswant Singh <jaswant432singh@gmail.com>
2022-03-09 00:10:12 +0530

    third commit

--- app.js
+++ app.js
@@ -59,10 +59,10 @@ app.post("/",function(req,res){
 
 
 app.post("/failure",function(req,res){
-    res.redirect("/")
+    res.redirect("/");
 });
 
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server stared on port 3000");
 })
